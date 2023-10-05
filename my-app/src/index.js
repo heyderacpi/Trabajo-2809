cd95ee0c55d3944eac7f89e06a9a07409b41365e
heyderacpi <141354703+heyderacpi@users.noreply.github.com>
2023-10-05 09:35:08 -0500

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Stock from "./nuevo";
+import { AppProvider } from "./AppContext";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <Stock />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
